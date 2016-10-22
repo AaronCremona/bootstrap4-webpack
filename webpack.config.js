d0960fb7f97844e2ae199de21f0ec3dbfff8d277
Aaron Cremona <dubbacremona@gmail.com>
2016-10-21 21:20:55 -0700

    yay bootstrap 4 with webpack

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  entry: {
+    main: "./src/index.js",
+  },
+  output: {
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      { test: /\.css$/, loader: "style!css" },
+      { test: /\.scss$/, loader: "style!css!sass" }
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.json', '.scss', '.css']
+  }
+}
